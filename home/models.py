f981d68733c3bf249721c932e9c9cf86aea1eecf
bkorayoz <bkorayoz@bkorayoz>
2017-03-09 22:18:53 +0300

    user html ve db baglantisi yapildi

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    userName = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.userName
--- home/views.py
+++ home/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from home.models import Users
+from django.template import Context, Template
 
-# Create your views here.
+def home(request):
+    return render(request, 'home/home.html')
+
+def statistics(request):
+     return render(request, 'home/statistics.html')
+ 
+def grading(request):
+     return render(request, 'home/grading.html')
+ 
+def users(request):
+    try:
+        users = Users.objects.values()
+        context = Context({'Users':users})
+        return render(request, 'home/users.html',context)
+    except Users.DoesNotExist:
+        return render(request, 'home/users.html')
+ 
+def addUser(request):
+     if request.method == 'POST':
+        enteredUserName = request.POST.get('userName', None)
+        try:
+            data = Users.objects.get(userName = enteredUserName)
+        except Users.DoesNotExist:
+            newUser = Users(userName = enteredUserName)
+            newUser.save()
+            return render(request, 'home/users.html')
+     else:
+        return render(request, 'home/users.html')
