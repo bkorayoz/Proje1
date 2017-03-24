afa135798f789f41cb455c3fc2f9b996292a03b6
turgut <turgut@161.9.13.16>
2017-03-24 14:50:19 +0300

    setting.py duzenlemesi

--- Proje1/settings.py
+++ Proje1/settings.py
@@ -11,12 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import dj_database_url
-db_from_env = dj_database_url.config()
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
